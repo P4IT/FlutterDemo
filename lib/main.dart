b616ea80ff59268328595cd00ce997733b3406f2
S-Hoppe <37839369+S-Hoppe@users.noreply.github.com>
2021-09-26 13:42:45 +0200

    update demo

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  var activeTL = false;
+  var _visible = false;
 
   @override
   void initState() {
@@ -19,73 +21,149 @@ class _MyAppState extends State<MyApp> {
   }
 
   void handleLicenseEvent(Map event) async {
-    if(event["body"]["FunctionName"].compareTo('onActivationResult') == 0) {
+    if (event["body"]["FunctionName"].compareTo('onActivationResult') == 0) {
       String value = await FlutterCidscan.decoderVersion();
       print(value);
     }
   }
 
   void handleInit(Map event) async {
-    if(event["body"]["FunctionName"].compareTo('initCaptureID') == 0) {
+    if (event["body"]["FunctionName"].compareTo('initCaptureID') == 0) {
       FlutterCidscan.activateEDKLicense(
-          'HetpZ4Bk0ivwF6dhHIHmbQH5ALUMOb/cQB5+I3fdD2LV5Yrm1sxkbrVL1X8vL4S55O7hmCQhGPCemfH/mUTz5yy3ip3TQDJU2pCeU3Zjbb3kyVdPYGdn59dPGFdv+aXdrIIRQHizRtbsC8NamuxWaRAGTfTGCM/IhEKvxkQO0ONSneBYSrY8OHpcEK7EVKCDSl3JQNJwHVWbpJDcIoR/WRVMSlJ/e9/qFOM3mPhwVJ1FCDF+e94mKpHQzOlRffJ/Zm2Mv1+UKJwriC/VpWK7+N1KJ9EZChf8U0MSdrMxSTGCxVZzZbTbt+pflWv2jUD9yFxHGbuyHVvpqnk0PUpFxF5VLIZy9kIesTg3ZSiho6kGTOY1bdGfqW3NppYmh1H0eCxQquhn5RitL74LwuLmKw==',
-          'P4I082220190001').listen((event) => { handleLicenseEvent(event) });
+              'HetpZ4Bk0ivwF6dhHIHmbQH5ALUMOb/cQB5+I3fdD2LV5Yrm1sxkbrVL1X8vL4S55O7hmCQhGPCemfH/mUTz5yy3ip3TQDJU2pCeU3Zjbb3kyVdPYGdn59dPGFdv+aXdrIIRQHizRtbsC8NamuxWaRAGTfTGCM/IhEKvxkQO0ONSneBYSrY8OHpcEK7EVKCDSl3JQNJwHVWbpJDcIoR/WRVMSlJ/e9/qFOM3mPhwVJ1FCDF+e94mKpHQzOlRffJ/Zm2Mv1+UKJwriC/VpWK7+N1KJ9EZChf8U0MSdrMxSTGCxVZzZbTbt+pflWv2jUD9yFxHGbuyHVvpqnk0PUpFxF5VLIZy9kIesTg3ZSiho6kGTOY1bdGfqW3NppYmh1H0eCxQquhn5RitL74LwuLmKw==',
+              'P4I082220190001')
+          .listen((event) => {handleLicenseEvent(event)});
+    }
+  }
+
+  void torchlight() {
+    if (!activeTL) {
+      activeTL = true;
+      FlutterCidscan.setTorch(true);
+    } else {
+      activeTL = false;
+      FlutterCidscan.setTorch(false);
     }
   }
 
   void handleDecode(Map event) {
     print(event);
-    if(event["body"]["FunctionName"].compareTo('receivedDecodedData') == 0) {
-
-    }
+    if (event["body"]["FunctionName"].compareTo('receivedDecodedData') == 0) {}
   }
 
-  void startDecode(){
-    FlutterCidscan.startDecoding().listen((event) => { handleDecode(event) });
+  void _toggle() {
+    if(_visible){
+      FlutterCidscan.closeCamera();
+    }
+    setState(() {
+      _visible = !_visible;
+    });
+  }
+
+  void startDecode() {
+    FlutterCidscan.startDecoding().listen((event) => {handleDecode(event)});
   }
 
   Future<void> init() async {
-    FlutterCidscan.initCaptureID('Wir benötigen die Kamera Berechtigung zum Scannen der Barcodes', false, true).listen((event) => { handleInit(event) });
+    FlutterCidscan.initCaptureID(
+            'Wir benötigen die Kamera Berechtigung zum Scannen der Barcodes',
+            false,
+            true)
+        .listen((event) => {handleInit(event)});
   }
+
+  clicker() {}
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-        appBar: AppBar(
-        title: const Text('Plugin example app'),
-    ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-
-            Row(
-              children: [
-                Column(),
-                Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      TextButton(
-                      onPressed: () => {init()},
-                      child: Text('Init')
+    return Stack(
+      alignment: Alignment.topCenter,
+      children: <Widget>[
+        Container(
+          color: Color(0xff619ff9),
+        ),
+        Visibility(
+          visible: _visible,
+          child: Container(
+            height: 400.0,
+            width: 400.0,
+            child: new Directionality(
+              textDirection: TextDirection.ltr,
+              child: CIDScanView(
+                onCIDScanViewCreated: null,
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          top: 70,
+          left: 30,
+          child: Container(
+            alignment: Alignment.topCenter,
+            height: 50,
+            width: 50,
+            child: Directionality(
+              textDirection: TextDirection.ltr,
+              child: ConstrainedBox(
+                  constraints: BoxConstraints.expand(),
+                  child: TextButton(
+                    onPressed: _toggle,
+                    child: Image(
+                      alignment: Alignment.center,
+                      matchTextDirection: true,
+                      image: NetworkImage(
+                          'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
                     ),
-                  ]
-                ),
-                Container(
-                  height: 200,
-                  width: 200,
-                  child: CIDScanView(
+                  )),
+            ),
+          ),
+        ),
+        Positioned(
+          top: 70,
+          right: 30,
+          child: Container(
+            alignment: Alignment.topCenter,
+            height: 50,
+            width: 50,
+            child: Directionality(
+              textDirection: TextDirection.ltr,
+              child: ConstrainedBox(
+                  constraints: BoxConstraints.expand(),
+                  child: TextButton(
+                    onPressed: torchlight,
+                    child: Image(
+                      alignment: Alignment.center,
+                      matchTextDirection: true,
+                      image: NetworkImage(
+                          'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+                    ),
+                  )),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 100,
+          child: Container(
+            alignment: Alignment.bottomCenter,
+            height: 50,
+            width: 150,
+            child: Directionality(
+              textDirection: TextDirection.ltr,
+              child: ConstrainedBox(
+                constraints: BoxConstraints.expand(),
+                child: TextButton(
+                  onPressed: _toggle,
+                  child: Text(
+                    'Starten',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.white),
                   ),
                 ),
-                TextButton(onPressed: () => {startDecode()}, child: Text('Decode')),
-                Column(),
-              ],
-            )
-          ],
+              ),
+            ),
+          ),
         ),
-      ),
-    ),
+      ],
     );
   }
 }
